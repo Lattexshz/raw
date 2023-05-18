84bb079b71e5e31195e8bac6e0e66dd30387a971
Lattexshz <125027502+Lattexshz@users.noreply.github.com>
2023-05-18 12:27:22 +0900

    Added some traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
-pub trait AsRaw {
-
+pub trait AsRaw<T> {
+    fn as_raw(&self) -> &T;
 }
 
-pub trait IntoRaw {
-    
+pub trait ToRaw<T> {
+    fn to_raw(&self) -> T;
+}
+
+pub trait IntoRaw<T> {
+    fn into_raw(self) -> T;
+}
+
+pub trait FromRaw<T> {
+    fn from_raw(obj:T) -> Self;
 }
